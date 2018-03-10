17bf89bb95e241ca0313e68d435519bcf31a3499
dhumdil <martin.peter.the.white@gmail.com>
2018-03-10 15:40:52 +0000

    [+] ...

--- src/layouts/login_layout.php
+++ src/layouts/login_layout.php
@@ -1,7 +1,8 @@
 
-<form action="/src/php/login.php" method="POST">
+<form class="form" action="/src/php/login.php" method="POST">
 	<input type="text" name="uid" placeholder="Username"><br>
 	<input type="password" name="pwd" placeholder="Password"><br>
 	<button type="submit">Login</button>
 </form>
+
 <a href="/src/layouts/create_user_layout.php">Create Account here</a>
--- src/php/dbh.php
+++ src/php/dbh.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "users"); //DB info (host,un,pwd,name of DB)
+$conn = mysqli_connect("http://rush_00.test", "root", "", "users"); // DB info (host,un,pwd,name of DB)
 
 // show up error message <use it just for test> if you dont want to be hacked ^_^
 if (!$conn)
