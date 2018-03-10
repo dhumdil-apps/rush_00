d519d5114c60dc0e3fdd17a625892894ccf3ca2e
adsabau <sabau.ad.adrian@gmail.com>
2018-03-10 15:17:11 +0200

    v0.01

---
+++ webStore/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./pages/login.php" method="POST">
+        <input type="text" name="uid" placeholder="Username"><br>
+        <input type="password" name="pwd" placeholder="Password"><br>
+        <button type="submit">Login</button>
+    </form>
+    <a href="./pages/createAccount.php">Create Account here</a>
+</body>
+</html>
---
+++ webStore/pages/login.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'dbh.php';
+
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+$sql = "SELECT * FROM users WHERE uid='$uid' AND pwd='$pwd'";
+$result = mysqli_query($conn, $sql);
+
+if (!$row = mysqli_fetch_assoc($result)){
+    echo("Username or Password is incorrect!");
+    header("Location: ../index.php");
+} else {
+    header("Location: ./main.html");
+}
