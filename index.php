8dc2b9d5c28252e76d78be4083cc43297529bd36
dhumdil <martin.peter.the.white@gmail.com>
2018-03-10 14:59:35 +0000

    [*] route fixing...

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	require('/src/php/connect.php');
+	require('./src/php/connect.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,14 +12,14 @@
 	<title>Rush00 - PHP Piscine</title>
 
 	<!-- CSS -->
-	<?php include "includes/links.php"?>
+	<?php include "./src/includes/links.php"?>
 </head>
 <body>
-	<?php include "layouts/header_layout.php"?>
+	<?php include "./src/layouts/header_layout.php"?>
 	<!-- TODO: read link and insert corresponding layout -->
-	<?php include "layouts/footer_layout.php"?>
+	<?php include "./src/layouts/footer_layout.php"?>
 
 	<!-- JavaScript -->
-	<?php include "includes/scripts.php"?>
+	<?php include "./src/includes/scripts.php"?>
 </body>
 </html>
