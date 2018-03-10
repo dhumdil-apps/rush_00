78c640cc733dad029bfaf94f7336ddf44053fbc1
dhumdil <martin.peter.the.white@gmail.com>
2018-03-10 16:42:08 +0000

    [+] ...

--- src/layouts/header_layout.php
+++ src/layouts/header_layout.php
@@ -1,3 +1,6 @@
 <div class="header layout-row">
 	<div class="title">Lorem ipsum dolor sit amet</div>
+	<div class="flex"></div>
+	<div class="login">LOGIN</div>
+	<div class="cart">CART</div>
 </div>
--- src/layouts/login_layout.php
+++ src/layouts/login_layout.php
@@ -1,43 +1,45 @@
-<form id="form" class="form" class="layout-column" method="POST" action="/src/php/login.php">
+<div class="login">
+	<form id="form" class="form" class="layout-column" method="POST" action="/src/php/login.php">
 
-	<div class="layout-row txt">
-		<div class="flex"></div>
-		<div class="p">
-			<p>Accusantium doloremque laudantium, totam rem aperiam</p>
-		</div>
-	</div>
-
-	<div class="layout-row input <?php echo (isset($errors['email'])) ? 'invalid-input' : '';  ?>">
-		<div class="flex"></div>
-		<label for="email">Email:</label>
-		<input id="email" type="email" name="email" value="<?php echo (isset($SESSION['email'])) ? $SESSION['email'] : ''; ?>" required autofocus onkeydown="submitForm(event, 'keyboard')">
-	</div>
-
-	<?php if (isset($errors['email'])) { ?>
-		<div class="layout-row input err">
+		<div class="layout-row txt">
 			<div class="flex"></div>
-			<span><?php echo $errors['email'][0]; ?></span>
+			<div class="p">
+				<p>Accusantium doloremque laudantium, totam rem aperiam</p>
+			</div>
 		</div>
-	<?php } ?>
 
-	<div class="layout-row input <?php echo (isset($errors['password'])) ? 'invalid-input' : '';  ?>">
-		<div class="flex"></div>
-		<label for="password">Password:</label>
-		<input id="password" type="password" name="password" required onkeydown="submitForm(event, 'keyboard')">
-	</div>
-
-	<?php if (isset($errors['password'])) { ?>
-		<div class="layout-row input err">
+		<div class="layout-row input <?php echo (isset($errors['email'])) ? 'invalid-input' : '';  ?>">
 			<div class="flex"></div>
-			<span><?php echo $errors['password'][0]; ?></span>
+			<label for="email">Email:</label>
+			<input id="email" type="email" name="email" value="<?php echo (isset($SESSION['email'])) ? $SESSION['email'] : ''; ?>" required autofocus onkeydown="submitForm(event, 'keyboard')">
 		</div>
-	<?php } ?>
 
-	<div class="layout-row">
-		<div class="flex"></div>
-		<div class="btn submit" onclick="submitForm(event, 'mouse')">
-			<span>Login</span>
+		<?php if (isset($errors['email'])) { ?>
+			<div class="layout-row input err">
+				<div class="flex"></div>
+				<span><?php echo $errors['email'][0]; ?></span>
+			</div>
+		<?php } ?>
+
+		<div class="layout-row input <?php echo (isset($errors['password'])) ? 'invalid-input' : '';  ?>">
+			<div class="flex"></div>
+			<label for="password">Password:</label>
+			<input id="password" type="password" name="password" required onkeydown="submitForm(event, 'keyboard')">
 		</div>
-	</div>
 
-</form>
+		<?php if (isset($errors['password'])) { ?>
+			<div class="layout-row input err">
+				<div class="flex"></div>
+				<span><?php echo $errors['password'][0]; ?></span>
+			</div>
+		<?php } ?>
+
+		<div class="layout-row">
+			<div class="flex"></div>
+			<div class="btn submit" onclick="submitForm(event, 'mouse')">
+				<span>Login</span>
+			</div>
+		</div>
+
+	</form>
+</div>
