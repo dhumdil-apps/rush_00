65549d28209d5305d628ee8c2e5e129a93b5b2e4
Lakatos Martin <dhumdil@gmail.com>
2018-03-11 23:35:42 +0200

    [+] .gitignore

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "ft_minishop";
+	$environment = "debug";
+
+	return '{"hostname":"'.$host.'","username":"'.$username.'","password":"'.$password.'","database":"'.$database.'","environment":"'.$environment.'"}';
