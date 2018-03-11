ca95808f350630d290d9b9fc3004cd8ab201b59c
Cristian Harangus <cristian.haragus@gmail.com>
2018-03-11 17:56:34 +0200

    installer in progress

---
+++ MySQL/12_add_categories.sql
@@ -0,0 +1,16 @@
+INSERT INTO categories (name)
+VALUES
+    ('Procesoare'),
+    ('Placi de baza'),
+    ('Placi video'),
+    ('Memorii'),
+    ('SSD'),
+    ('HDD'),
+    ('Surse'),
+    ('Carcase'),
+    ('Coolere'),
+    ('Ventilatoare'),
+    ('Modding'),
+    ('Placi sunet'),
+    ('DVD / BlueRay'),
+    ('Tv Tuner');
