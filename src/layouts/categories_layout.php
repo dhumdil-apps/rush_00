3916aebab6979f27cd9045826f630591e2ef8463
Cristian Harangus <cristian.haragus@gmail.com>
2018-03-11 23:33:44 +0200

    futui

--- src/layouts/categories_layout.php
+++ src/layouts/categories_layout.php
@@ -1,22 +1,29 @@
+<?php
+    require('./src/php/db_connect.php');
+
+        // $product = mysqli_real_escape_string($link, $email);
+		$sql = "SELECT id, name FROM categories";
+?>
+
 <form class ="main-container layout-center" id="form" method="GET" action="index.php">
 
 	<input id="url" class="hidden" type="text" name="url" value="">
 	<input id="id" class="hidden" type="text" name="category" value="">
 
-	<div class="items layout-column">
-		<div class="item layout-row" onclick="submitForm(event, 'mouse', 'products', 'category1')">
-			<span class="layout-center">CATEGORY 1</span>
-		</div>
-		<div class="item layout-row" onclick="submitForm(event, 'mouse', 'products', 'category1')">
-			<span class="layout-center">CATEGORY 1</span>
-		</div>
-		<div class="item layout-row" onclick="submitForm(event, 'mouse', 'products', 'category1')">
-			<span class="layout-center">CATEGORY 1</span>
-		</div>
-		<div class="item layout-row" onclick="submitForm(event, 'mouse', 'products', 'category1')">
-			<span class="layout-center">CATEGORY 1</span>
-		</div>
+<?php 
+        foreach ($result = mysqli_fetch_all(sqlQuery($sql)) as $row){
+?>
 
-	
+	<div class="items layout-column">
+		<div class="item layout-row" onclick="submitForm(event, 'mouse', 'products', '<?php echo($row[0]);?>')">
+			<span class="layout-center"><?php echo($row[1]);?></span>
+		</div>	
 	</div>
+
+
+<?php
+		};
+?>
+
+
 </form>
--- src/layouts/product-list_layout.php
+++ src/layouts/product-list_layout.php
@@ -1,3 +1,28 @@
+<?php
+    require('./src/php/db_connect.php');
+
+    if(array_key_exists("pid", $_GET)){
+        // $product = mysqli_real_escape_string($link, $email);
+        // $sql = "SELECT name FROM categories";
+        // $result = mysqli_fetch_all(sqlQuery($sql))[0];
+        // echo"<pre>";
+        // var_dump($result);
+        // echo "</pre>";
+
+        // $pId = $_GET['pid'];
+        // $pName = $result[1];
+        // $pCatId = $result[2];
+        // $pStock = $result[3];
+        // $pPrice = $result[4];
+        // $pDescription = $result[5];
+        // $pImg = $result[6];
+        
+        // $productId = "...";
+
+    }
+?>
+
+
 <form id="form" method="GET" action="index.php">
 
 	<input id="url" class="hidden" type="text" name="url" value="">
