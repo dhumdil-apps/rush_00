070a3898dcdf2241e67411396b24cd928c808f45
Cristian Harangus <cristian.haragus@gmail.com>
2018-03-11 19:38:49 +0200

    some minor thing for env

--- config.php
+++ config.php
@@ -5,24 +5,3 @@
     $database = "rush00";
 
     return '{"hostname":"'.$host.'","username":"'.$username.'","password":"'.$password.'","database":"'.$database.'"}';
-
-    if (isset($config['hostname'])) {
-        $hn = "value = '".$config['hostname']."'";
-      } else {
-        $hn = "placeholder = 'enter hostname here'";
-      }
-      if (isset($config['username'])) {
-        $un = "value = '".$config['username']."'";
-      } else {
-        $un = "placeholder = 'enter username here'";
-      }
-      if (isset($config['password'])) {
-        $pw = "value = '".$config['password']."'";
-      } else {
-        $pw = "placeholder = 'enter password here'";
-      }
-      if (isset($config['database'])) {
-        $db = "value = '".$config['database']."'";
-      } else {
-        $db = "placeholder = 'enter database here'";
-      }
--- src/php/init.php
+++ src/php/init.php
@@ -1,13 +1,18 @@
 <?php
-
-	$environment = getenv('APP_ENV');
-
+	(@ $config = include_once 'config.php') or die ("There was a problem with you config file. Check it and try again");
+	$config = json_decode($config,TRUE);
+	
+	$_ENV['hostname']=$config['hostname'];
+	$_ENV['username']=$config['username'];
+	$_ENV['password']=$config['password'];
+	$_ENV['database']=$config['database'];
+	
 	if ($environment == 'debug')
 	{
 		error_reporting(E_ALL);
 		ini_set('display_errors', 1);
 	}
 
-	session_start();
+	
 
 	require('./src/php/routes.php');
